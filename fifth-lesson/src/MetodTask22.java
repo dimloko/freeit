3be04350874d29617abfa22d305b50bbd6258d5e
dimloko <logvinenko.d.a@mail.ru>
2020-08-08 12:43:48 +0300

    Homework for lesson5

---
+++ fifth-lesson/src/MetodTask22.java
@@ -0,0 +1,19 @@
+/*Написать программу, выполняющую поиск в строке шестнадцатеричных чисел,
+записаных по правилам Java, с помощью регулярных выражений и вывести их на
+страницу.*/
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MetodTask22 {
+    public static void main(String[] args) {
+        String str = "Red-0xFF0000, Yellow - 0xFFFF00, DarkGoldenRod - 0xB8860B";  //Исходная строка
+        Pattern pat = Pattern.compile("[0]{1}[xX]{1}[0-9[a-fA-F]]{6}"); //Компиляция представления регулярного выражения
+        Matcher mat = pat.matcher(str); //Создание объекта Matcher
+
+        //Поиск и вывод совпадений в строке
+        while (mat.find()) {
+            System.out.println(mat.group());
+        }
+    }
+}
